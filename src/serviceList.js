3167eed2e2be7e86492c399109983f6f53cc7856
gkorbakis <gkorbakis@epu.ntua.gr>
2024-01-24 13:59:18 +0200

    ServicePage.js: updates

--- src/serviceList.js
+++ src/serviceList.js
@@ -4,13 +4,17 @@ export const serviceList = [
         "title": "Service A",
         "developer": "Developer A",
         "short_description": "Lorem ipsum dolor sit amet, consectetur adipiscing elit.",
-        "full_description": "Lorem ipsum dolor sit amet, consectetur adipiscing elit. Sed vel sem nec leo aliquet commodo ac a felis. Nullam eget elit id dolor facilisis hendrerit. In hac habitasse platea dictumst.",
-        "service_links": ["https://example.com/service_a"],
-        "github_links": [
-            "https://github.com/developerA/service_a",
-            "https://github.com/developerA/service_a-frontend"
+        "full_description": "Lorem ipsum dolor sit amet, consectetur adipiscing elit. Sed vel sem nec leo aliquet commodo ac a felis. Nullam eget elit id dolor facilisis hendrerit. In hac habitasse platea dictumst. Sed vel sem nec leo aliquet commodo ac a felis. Nullam eget elit id dolor facilisis hendrerit. In hac habitasse platea dictumst.",
+        "service_links": [
+            { "name": "Service A Link", "link": "https://example.com/service_a" }
         ],
-        "aiod_links": ["https://aiodlink.com/service_a"],
+        "github_links": [
+            { "name": "Service A GitHub", "link": "https://github.com/developerA/service_a" },
+            { "name": "Service A Frontend GitHub", "link": "https://github.com/developerA/service_a-frontend" }
+        ],
+        "aiod_links": [
+            { "name": "Service A AIOD Link", "link": "https://aiodlink.com/service_a" }
+        ],
         "image": "https://via.placeholder.com/400x300"
     },
     {
@@ -18,13 +22,17 @@ export const serviceList = [
         "title": "Service B",
         "developer": "Developer B",
         "short_description": "Lorem ipsum dolor sit amet, consectetur adipiscing elit. Sed vel sem nec leo aliquet commodo ac a felis.",
-        "full_description": "Lorem ipsum dolor sit amet, consectetur adipiscing elit. Sed vel sem nec leo aliquet commodo ac a felis. Nullam eget elit id dolor facilisis hendrerit.",
-        "service_links": ["https://example.com/service_b"],
-        "github_links": [
-            "https://github.com/developerB/service_b",
-            "https://github.com/developerB/service_b-frontend"
+        "full_description": "Lorem ipsum dolor sit amet, consectetur adipiscing elit. Sed vel sem nec leo aliquet commodo ac a felis. Nullam eget elit id dolor facilisis hendrerit. Sed vel sem nec leo aliquet commodo ac a felis. Nullam eget elit id dolor facilisis hendrerit.",
+        "service_links": [
+            { "name": "Service B Link", "link": "https://example.com/service_b" }
         ],
-        "aiod_links": ["https://aiodlink.com/service_b"],
+        "github_links": [
+            { "name": "Service B GitHub", "link": "https://github.com/developerB/service_b" },
+            { "name": "Service B Frontend GitHub", "link": "https://github.com/developerB/service_b-frontend" }
+        ],
+        "aiod_links": [
+            { "name": "Service B AIOD Link", "link": "https://aiodlink.com/service_b" }
+        ],
         "image": "https://via.placeholder.com/400x300"
     },
     {
@@ -32,13 +40,17 @@ export const serviceList = [
         "title": "Service C",
         "developer": "Developer C",
         "short_description": "Lorem ipsum dolor sit amet, consectetur adipiscing elit. Nullam eget elit id dolor facilisis hendrerit.",
-        "full_description": "Lorem ipsum dolor sit amet, consectetur adipiscing elit. Nullam eget elit id dolor facilisis hendrerit. In hac habitasse platea dictumst. Sed vel sem nec leo aliquet commodo ac a felis.",
-        "service_links": ["https://example.com/service_c"],
-        "github_links": [
-            "https://github.com/developerC/service_c",
-            "https://github.com/developerC/service_c-frontend"
+        "full_description": "Lorem ipsum dolor sit amet, consectetur adipiscing elit. Nullam eget elit id dolor facilisis hendrerit. In hac habitasse platea dictumst. Sed vel sem nec leo aliquet commodo ac a felis. Nullam eget elit id dolor facilisis hendrerit. In hac habitasse platea dictumst.",
+        "service_links": [
+            { "name": "Service C Link", "link": "https://example.com/service_c" }
         ],
-        "aiod_links": ["https://aiodlink.com/service_c"],
+        "github_links": [
+            { "name": "Service C GitHub", "link": "https://github.com/developerC/service_c" },
+            { "name": "Service C Frontend GitHub", "link": "https://github.com/developerC/service_c-frontend" }
+        ],
+        "aiod_links": [
+            { "name": "Service C AIOD Link", "link": "https://aiodlink.com/service_c" }
+        ],
         "image": "https://via.placeholder.com/400x300"
     }
-]
+];
