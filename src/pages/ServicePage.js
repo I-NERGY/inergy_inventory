7439f34a3ba9988a272d06f5dbb80efa469b8e0a
gkorbakis <gkorbakis@epu.ntua.gr>
2024-01-24 14:58:35 +0200

    ServicePage.js: screenshots added

--- src/pages/ServicePage.js
+++ src/pages/ServicePage.js
@@ -1,3 +1,5 @@
+import React, {useState} from 'react';
+
 import {useParams, Link} from "react-router-dom";
 import {serviceList} from "../serviceList";
 
@@ -8,12 +10,35 @@ import Paper from "@mui/material/Paper";
 import Container from "@mui/material/Container";
 import Grid from "@mui/material/Grid";
 import Stack from "@mui/material/Stack";
+import Dialog from '@mui/material/Dialog';
+import DialogActions from '@mui/material/DialogActions';
+import DialogContent from '@mui/material/DialogContent';
+import DialogTitle from '@mui/material/DialogTitle';
 
 import ChevronRightIcon from '@mui/icons-material/ChevronRight';
+import ArrowForwardIcon from '@mui/icons-material/ArrowForward';
+import ArrowBackIcon from '@mui/icons-material/ArrowBack';
+
 
 import Breadcrumb from "../components/layout/Breadcrumb";
 import {Button, useTheme} from "@mui/material";
-import React from "react";
+
+// Sample image URLs
+const imageUrls = [
+    {
+        "name": "Image 1",
+        "source": "https://i-nergy.eu/sites/default/files/styles/mt_large/public/2023-12/i-nergy_3_0_0.jpg?itok=JZDBUS_x"
+    },
+    {
+        "name": "Image 2",
+        "source": "https://i-nergy.eu/sites/default/files/styles/mt_large/public/2023-10/Fig.1.png?itok=C5GPz1n0"
+    },
+    {"name": "Image 3", "source": "https://pbs.twimg.com/media/GBYSqazXgAA-pR1.jpg"},
+    {
+        "name": "Image 4",
+        "source": "https://i-nergy.eu/sites/default/files/styles/mt_large/public/2023-07/31_I-NERGY%20blog%20article_FBA_31072023.png?itok=ld__E---"
+    }
+]
 
 const ServicePage = () => {
     const theme = useTheme();
@@ -26,16 +51,27 @@ const ServicePage = () => {
             {service?.title}
         </Typography>];
 
-    const gradientBackgroundAIoD = `linear-gradient(to right, #0047BB, #E7DC56)`;
-    const gradientTextAIoD = `linear-gradient(to right, #E7DC56, #0047BB)`;
-    const gradientTextStyleAIoD = {
-        background: gradientTextAIoD,
-        '-webkit-background-clip': 'text',
-        color: 'transparent', // Hide the original text
-        display: 'inline-block',
-        // fontWeight: 'bold'
+    const [open, setOpen] = useState(false);
+    const [selectedImage, setSelectedImage] = useState('');
+
+    const [selectedImageIndex, setSelectedImageIndex] = useState(0);
+
+    const handleOpen = (index) => {
+        setSelectedImageIndex(index);
+        setOpen(true);
     };
 
+    const handleClose = () => {
+        setOpen(false);
+    };
+
+    const handlePrevImage = () => {
+        setSelectedImageIndex((prevIndex) => (prevIndex - 1 + imageUrls.length) % imageUrls.length);
+    };
+
+    const handleNextImage = () => {
+        setSelectedImageIndex((prevIndex) => (prevIndex + 1) % imageUrls.length);
+    };
 
     return (
         <>
@@ -69,7 +105,7 @@ const ServicePage = () => {
 
                             <img
                                 style={{height: '100%', maxHeight: '400px', width: '100%', objectFit: 'cover',}}
-                                src={service.image} className={'serviceImage'}
+                                src={service.image.source} className={'serviceImage'}
                                 alt=""/>
                         </Grid>
                     </Grid>
@@ -182,27 +218,118 @@ const ServicePage = () => {
                                         </Typography>
                                     </Button>
                                 ))}
-                                {/*{service.aiod_links.map(link => (*/}
-                                {/*    <Button*/}
-                                {/*        key={link.link}*/}
-                                {/*        component={Link}*/}
-                                {/*        to={link.link}*/}
-                                {/*        size="medium"*/}
-                                {/*        variant="outlined"*/}
-                                {/*        sx={{*/}
-                                {/*            background: gradientBackgroundAIoD*/}
-                                {/*        }}*/}
-                                {/*    >*/}
-                                {/*        <Typography variant="body1" style={gradientTextStyleAIoD}>*/}
-                                {/*            {link.name}*/}
-                                {/*        </Typography>*/}
-                                {/*    </Button>*/}
-                                {/*))}*/}
                             </Box>
                         </Stack>}
 
                     </Container>}
                 </Paper>
+
+                {service.screenshots?.length > 0 && <>
+                    <Box display={'flex'} sx={{justifyContent: 'space-between', px: {xs: 0, md: 2}}} mt={5}>
+                        <Typography fontWeight={'bold'} variant={'h5'}>Screenshots</Typography>
+                    </Box>
+
+                    <Divider sx={{mt: 1}}/>
+                </>}
+
+                <div>
+                    <Grid container spacing={2} mt={1}>
+                        {service.screenshots?.map((image, index) => (
+                            <Grid item key={index} xs={6} md={4} style={{height: '300px'}}>
+                                <div
+                                    style={{
+                                        position: 'relative',
+                                        height: '100%',
+                                    }}
+                                >
+                                    <img
+                                        src={image.source}
+                                        alt={image.name}
+                                        style={{
+                                            width: '100%',
+                                            height: '100%',
+                                            objectFit: 'cover',
+                                            cursor: 'pointer',
+                                            transition: 'opacity 0.3s',
+                                            ':hover': {
+                                                opacity: 0.7,
+                                            },
+                                        }}
+                                        onClick={() => handleOpen(index)}
+                                    />
+                                </div>
+                            </Grid>
+                        ))}
+                    </Grid>
+
+                    <Dialog open={open} onClose={handleClose} maxWidth={'xl'}>
+                        <DialogTitle>Image Preview</DialogTitle>
+                        <DialogContent>
+                            <div
+                                style={{
+                                    position: 'relative',
+                                    display: 'flex',
+                                    justifyContent: 'center',
+                                }}
+                            >
+                                <img
+                                    src={imageUrls[selectedImageIndex].source}
+                                    alt={`Selected ${selectedImageIndex + 1}`}
+                                    style={{maxWidth: '100%', maxHeight: '100%', margin: '0 auto'}}
+                                />
+                                <div
+                                    style={{
+                                        position: 'absolute',
+                                        top: '50%',
+                                        transform: 'translateY(-50%)',
+                                        left: '32px',
+                                        cursor: 'pointer',
+                                        zIndex: 1,
+                                    }}
+                                    onClick={handlePrevImage}
+                                >
+                                    <ArrowBackIcon
+                                        fontSize="large"
+                                        color="primary"
+                                        style={{
+                                            fontSize: '3rem',
+                                            padding: '12px',
+                                            background: 'rgba(255, 255, 255, 0.7)',
+                                            borderRadius: '50%',
+                                        }}
+                                    />
+                                </div>
+                                <div
+                                    style={{
+                                        position: 'absolute',
+                                        top: '50%',
+                                        transform: 'translateY(-50%)',
+                                        right: '32px',
+                                        cursor: 'pointer',
+                                        zIndex: 1,
+                                    }}
+                                    onClick={handleNextImage}
+                                >
+                                    <ArrowForwardIcon
+                                        fontSize="large"
+                                        color="primary"
+                                        style={{
+                                            fontSize: '3rem',
+                                            padding: '12px',
+                                            background: 'rgba(255, 255, 255, 0.7)',
+                                            borderRadius: '50%',
+                                        }}
+                                    />
+                                </div>
+                            </div>
+                        </DialogContent>
+                        <DialogActions>
+                            <Button onClick={handleClose} color="primary">
+                                Close
+                            </Button>
+                        </DialogActions>
+                    </Dialog>
+                </div>
 
             </Container>
         </>
--- src/serviceList.js
+++ src/serviceList.js
@@ -4,53 +4,56 @@ export const serviceList = [
         "title": "Service A",
         "developer": "Developer A",
         "short_description": "Lorem ipsum dolor sit amet, consectetur adipiscing elit.",
-        "full_description": "Lorem ipsum dolor sit amet, consectetur adipiscing elit. Sed vel sem nec leo aliquet commodo ac a felis. Nullam eget elit id dolor facilisis hendrerit. In hac habitasse platea dictumst. Sed vel sem nec leo aliquet commodo ac a felis. Nullam eget elit id dolor facilisis hendrerit. In hac habitasse platea dictumst.",
-        "service_links": [
-            { "name": "Service A Link", "link": "https://example.com/service_a" }
-        ],
+        "full_description": "Lorem ipsum dolor sit amet, consectetur adipiscing elit. Sed vel sem nec leo aliquet commodo ac a felis. Nullam eget elit id dolor facilisis hendrerit. In hac habitasse platea dictumst. " +
+            "Sed vel sem nec leo aliquet commodo ac a felis. Nullam eget elit id dolor facilisis hendrerit. In hac habitasse platea dictumst. " +
+            "Sed vel sem nec leo aliquet commodo ac a felis. Nullam eget elit id dolor facilisis hendrerit. In hac habitasse platea dictumst. " +
+            "Sed vel sem nec leo aliquet commodo ac a felis. Nullam eget elit id dolor facilisis hendrerit. In hac habitasse platea dictumst.",
+        "service_links": [{"name": "Service A Link", "link": "https://example.com/service_a"}],
         "github_links": [
-            { "name": "Service A GitHub", "link": "https://github.com/developerA/service_a" },
-            { "name": "Service A Frontend GitHub", "link": "https://github.com/developerA/service_a-frontend" }
+            {"name": "GitHub Repository", "link": "https://github.com/developerA/service_a"},
+            {"name": "Frontend GitHub Repository", "link": "https://github.com/developerA/service_a-frontend"}
         ],
-        "aiod_links": [
-            { "name": "Service A AIOD Link", "link": "https://aiodlink.com/service_a" }
-        ],
-        "image": "https://via.placeholder.com/400x300"
+        "aiod_links": [{"name": "AIOD Link", "link": "https://aiodlink.com/service_a"}],
+        "image": {"name": "Service A Image", "source": "https://via.placeholder.com/400x300"},
+        "screenshots": [
+            {"name": "Image 1", "source": "https://i-nergy.eu/sites/default/files/styles/mt_large/public/2023-12/i-nergy_3_0_0.jpg?itok=JZDBUS_x"},
+            {"name": "Image 2", "source": "https://i-nergy.eu/sites/default/files/styles/mt_large/public/2023-10/Fig.1.png?itok=C5GPz1n0"},
+            {"name": "Image 3", "source": "https://pbs.twimg.com/media/GBYSqazXgAA-pR1.jpg"},
+            {"name": "Image 4", "source": "https://i-nergy.eu/sites/default/files/styles/mt_large/public/2023-07/31_I-NERGY%20blog%20article_FBA_31072023.png?itok=ld__E---"}
+        ]
     },
     {
         "id": "service_b",
         "title": "Service B",
         "developer": "Developer B",
         "short_description": "Lorem ipsum dolor sit amet, consectetur adipiscing elit. Sed vel sem nec leo aliquet commodo ac a felis.",
-        "full_description": "Lorem ipsum dolor sit amet, consectetur adipiscing elit. Sed vel sem nec leo aliquet commodo ac a felis. Nullam eget elit id dolor facilisis hendrerit. Sed vel sem nec leo aliquet commodo ac a felis. Nullam eget elit id dolor facilisis hendrerit.",
-        "service_links": [
-            { "name": "Service B Link", "link": "https://example.com/service_b" }
-        ],
+        "full_description": "Lorem ipsum dolor sit amet, consectetur adipiscing elit. Sed vel sem nec leo aliquet commodo ac a felis. Nullam eget elit id dolor facilisis hendrerit. " +
+            "Lorem ipsum dolor sit amet, consectetur adipiscing elit. Sed vel sem nec leo aliquet commodo ac a felis. Nullam eget elit id dolor facilisis hendrerit. " +
+            "Lorem ipsum dolor sit amet, consectetur adipiscing elit. Sed vel sem nec leo aliquet commodo ac a felis. Nullam eget elit id dolor facilisis hendrerit. " +
+            "Lorem ipsum dolor sit amet, consectetur adipiscing elit. Sed vel sem nec leo aliquet commodo ac a felis. Nullam eget elit id dolor facilisis hendrerit.",
+        "service_links": [{"name": "Service B Link", "link": "https://example.com/service_b"}],
         "github_links": [
-            { "name": "Service B GitHub", "link": "https://github.com/developerB/service_b" },
-            { "name": "Service B Frontend GitHub", "link": "https://github.com/developerB/service_b-frontend" }
+            {"name": "GitHub Repository", "link": "https://github.com/developerB/service_b"},
+            {"name": "Frontend GitHub Repository", "link": "https://github.com/developerB/service_b-frontend"}
         ],
-        "aiod_links": [
-            { "name": "Service B AIOD Link", "link": "https://aiodlink.com/service_b" }
-        ],
-        "image": "https://via.placeholder.com/400x300"
+        "aiod_links": [{"name": "AIOD Link", "link": "https://aiodlink.com/service_b"}],
+        "image": {"name": "Service B Image", "source": "https://via.placeholder.com/400x300"}
     },
     {
         "id": "service_c",
         "title": "Service C",
         "developer": "Developer C",
         "short_description": "Lorem ipsum dolor sit amet, consectetur adipiscing elit. Nullam eget elit id dolor facilisis hendrerit.",
-        "full_description": "Lorem ipsum dolor sit amet, consectetur adipiscing elit. Nullam eget elit id dolor facilisis hendrerit. In hac habitasse platea dictumst. Sed vel sem nec leo aliquet commodo ac a felis. Nullam eget elit id dolor facilisis hendrerit. In hac habitasse platea dictumst.",
-        "service_links": [
-            { "name": "Service C Link", "link": "https://example.com/service_c" }
-        ],
+        "full_description": "Lorem ipsum dolor sit amet, consectetur adipiscing elit. Nullam eget elit id dolor facilisis hendrerit. In hac habitasse platea dictumst. " +
+            "Lorem ipsum dolor sit amet, consectetur adipiscing elit. Nullam eget elit id dolor facilisis hendrerit. In hac habitasse platea dictumst. " +
+            "Lorem ipsum dolor sit amet, consectetur adipiscing elit. Nullam eget elit id dolor facilisis hendrerit. In hac habitasse platea dictumst. " +
+            "Lorem ipsum dolor sit amet, consectetur adipiscing elit. Nullam eget elit id dolor facilisis hendrerit. In hac habitasse platea dictumst.",
+        "service_links": [{"name": "Service C Link", "link": "https://example.com/service_c"}],
         "github_links": [
-            { "name": "Service C GitHub", "link": "https://github.com/developerC/service_c" },
-            { "name": "Service C Frontend GitHub", "link": "https://github.com/developerC/service_c-frontend" }
+            {"name": "GitHub Repository", "link": "https://github.com/developerC/service_c"},
+            {"name": "Frontend GitHub Repository", "link": "https://github.com/developerC/service_c-frontend"}
         ],
-        "aiod_links": [
-            { "name": "Service C AIOD Link", "link": "https://aiodlink.com/service_c" }
-        ],
-        "image": "https://via.placeholder.com/400x300"
+        "aiod_links": [{"name": "AIOD Link", "link": "https://aiodlink.com/service_c"}],
+        "image": {"name": "Service C Image", "source": "https://via.placeholder.com/400x300"}
     }
 ];
